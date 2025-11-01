750ba826860a614d2f2c44a708067a5121730cee
Kemisdan <34918602+Kemisdan@users.noreply.github.com>
2025-10-31 22:32:12 -0400

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ import Database from "better-sqlite3";
 import fetch from "node-fetch";
 
 const app = express();
-const PORT = process.env.PORT || 5000; // fallback to 5000 for local dev
+const PORT = process.env.PORT || 5000; // fallback for local dev
 app.listen(PORT, () => {
   console.log(`🚀 Server running on port ${PORT}`);
 });
@@ -311,6 +311,7 @@ app.listen(PORT, () => {
   console.log(`🚀 Server running on http://localhost:${PORT}`);
 
 });
+
 
 
 
